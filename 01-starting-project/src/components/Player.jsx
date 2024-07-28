cc2e1e8be5d78f6c7db96c9fa96565f772d4acca
shouvik sinha <shouviksinha901@gmail.com>
2024-07-29 02:34:38 +0530

    user name

--- 01-starting-project/src/components/Player.jsx
+++ 01-starting-project/src/components/Player.jsx
@@ -1,9 +1,20 @@
+import {useState} from "react";
 export default function Player() {
+  const [userName, setUserName] = useState("champ");
+  // const [save, setSave] = useState(false);
+  // function handleSave(){
+  //   if(save == false){
+  //     setSave = true;
+  //   }
+  // }
+  function handleUserName (event){
+    setUserName(event.target.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName}</h2>
       <p>
-        <input type="text" />
+        <input type="text" value="Enter name" onChange={handleUserName} />
         <button>Set Name</button>
       </p>
     </section>
